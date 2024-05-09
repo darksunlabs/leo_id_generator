45f4407005b6a7375a0ff154bc9f48507aa0b5cf
harshnambiar <harrisonlvl2@gmail.com>
2024-05-09 14:18:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leo ID Generator
+
+Leo doesn't have a string type so it can sometimes be difficult to generate IDs such as transaction IDs that reflect the sender and the receiver and the timestamp of the operation. This code snippet is one way to resolve this issue by converting these details into an Aleo Address form, that can be readily used in Aleo Smart Contracts.
---
+++ index.js
@@ -0,0 +1,23 @@
+// aleo ids are of the form: aleo1vgdqhep7fpflj0yslgv05nflv7xdh5sexfvqpe6qav5g56x86grsr8l8m8
+
+const to = 'aleo1vgdqhep7fpflj0yslgv05nflv7xdh5sexfvqpe6qav5g56x86grsr8l8m8';
+const from = 'aleo1zjk0syd9jfkedc2tjklhcfc4qqmnyep3v393twywuya08yh4dgzq8wx8jd';
+const count = 13; //assuming the user has generated 13 ids before
+var now = Date.now().toString();
+while (now.length < 15){
+    now = '0'.concat(now);
+}
+var count_str = count.toString();
+while (count_str.length < 6){
+    count_str = '0'.concat(count_str);
+}
+
+console.log(to.length);
+console.log(from.length);
+console.log(now);
+
+
+
+const id = 'aleo'.concat(from.substring(5,24).concat(count_str).concat(to.substring(37,56)).concat(now));
+console.log(id);
+console.log(id.length);
