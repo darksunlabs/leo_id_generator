52621f8765fe2e3d95581ae5a954d6b9f6803e7b
harshnambiar <harrisonlvl2@gmail.com>
2024-05-13 19:04:18 +0530

    added a field id generator method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const count = 13; //assuming the user has generated 13 ids before
                     // this requires a mapping to exist to keep a track of such count 
                     // for every user
 
-function getLeoId(to, from, count){
+function getLeoIdAddress(to, from, count){
     var now = Date.now().toString();
     while (now.length < 15){
         now = '0'.concat(now);
@@ -16,15 +16,51 @@ function getLeoId(to, from, count){
         count_str = '0'.concat(count_str);
     }
 
-    console.log(to.length);
-    console.log(from.length);
-    console.log(now);
-
-
-
     const id = 'aleo'.concat(from.substring(5,24).concat(count_str).concat(to.substring(37,56)).concat(now));
     return id;
 }
 
-const id = getLeoId(to,from, count);
-console.log(id);
+function getLeoIdField(to, from, count){
+    var now = Date.now().toString();
+    while (now.length < 15){
+        now = '0'.concat(now);
+    }
+    var count_str = count.toString();
+    while (count_str.length < 6){
+        count_str = '0'.concat(count_str);
+    }
+
+    var res = "";
+    var i = 9;
+    while (i < 18){
+        var c = (from.charCodeAt(i)).toString();
+        while (c.length < 3){
+            c = '0'.concat(c);
+        }
+        
+        res = res.concat(c);
+        i += 1;
+    }
+    res = res.concat(count_str);
+
+    i = 29;
+    while (i < 38){
+        var c = (from.charCodeAt(i)).toString();
+        while (c.length < 3){
+            c = '0'.concat(c);
+        }
+        
+        res = res.concat(c);
+        i += 1;
+    }
+
+    res = res.concat(now);
+
+    return res.concat('field');
+}
+
+const id_a = getLeoIdAddress(to,from, count);
+console.log(id_a);
+
+const id_f = getLeoIdField(to, from, count);
+console.log(id_f)
